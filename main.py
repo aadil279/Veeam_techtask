49ec8c53499a3344edd865ef5a38b5133309434f
Aadil Sidik <94676398+aadil279@users.noreply.github.com>
2023-10-17 17:40:12 +0100

    feat: can now read attributes from command line refactor: made code more legible

--- main.py
+++ main.py
@@ -1,72 +1,108 @@
 import hashlib
 import os
+import sys
 import time
 import shutil
+from datetime import datetime
 
-replica_path = os.path.join("replica")
-source_path = os.path.join("source")
-log_path = os.path.join("operations.log")
-sync_interval = 5   #in seconds
+REPLICA_PATH = None
+SOURCE_PATH = None
+LOG_PATH = None
+SYNC_INTERVAL = None  # In seconds
+
 
 def main():
+    init_vars()
     while True:
-        compareFolders()
-        time.sleep(sync_interval)
+        sync_folders()
+        time.sleep(SYNC_INTERVAL)
 
-def hashfile(file):
-    BUF_SIZE = 65536    #Buffer size for file reading (64kB)
+
+# Initializes global attributes values, using input arguments from command line
+def init_vars():
+    if len(sys.argv) < 5:
+        print("Please insert all 4 arguments (replica_path, source_path, log_path, sync_interval)")
+        exit()
+    global REPLICA_PATH
+    global SOURCE_PATH
+    global LOG_PATH
+    global SYNC_INTERVAL
+
+    if not (os.path.isdir(sys.argv[1]) and os.path.isdir(sys.argv[2]) and os.path.isfile(sys.argv[3])):
+        print("Please insert valid paths for replica folder, source folder and log file")
+        exit()
+
+    REPLICA_PATH = sys.argv[1]
+    SOURCE_PATH = sys.argv[2]
+    LOG_PATH = sys.argv[3]
+    try:
+        SYNC_INTERVAL = float(sys.argv[4])
+    except ValueError:
+        print("Invalid value for synchronization interval")
+
+
+# Checks both folders and corrects inconsistencies
+def sync_folders():
+    source_files = os.listdir(SOURCE_PATH)
+    replica_files = os.listdir(REPLICA_PATH)
+
+    for file in source_files:
+        if os.path.isfile(os.path.join(SOURCE_PATH, file)):
+
+            if file not in replica_files:  # If the file does not exist in replica, copy to it
+                replace_file(file)
+                log("Added file " + file + " in replica folder")
+
+            else:  # If the file exists in replica but isn't the same as in source, replace it
+                if not is_equal_file(os.path.join(SOURCE_PATH, file), os.path.join(REPLICA_PATH, file)):
+                    replace_file(file)
+                    log("Replaced file " + file + " in replica folder with updated version from source folder.")
+
+    for file in replica_files:
+        if file not in source_files and os.path.isfile(os.path.join(REPLICA_PATH, file)):  # If file is in replica but not in source, it is an old deleted file
+            os.remove(os.path.join(REPLICA_PATH, file))
+            log("Removed file " + file + " from replica folder as it is no longer in source")
+
+
+def replace_file(file):
+    source_file_path = os.path.join(SOURCE_PATH, file)
+    replica_file_path = os.path.join(REPLICA_PATH, file)
+    if os.path.exists(replica_file_path):
+        os.remove(replica_file_path)
+    shutil.copy2(source_file_path, REPLICA_PATH)
+
+
+# Hashes file using MD5 for later comparison
+def hash_file(file):
+    buf_size = 65536  # Buffer size for file reading (64kB)
     md5 = hashlib.md5()
 
     with open(file, 'rb') as f:
         while True:
-            data = f.read(BUF_SIZE)
+            data = f.read(buf_size)
 
             if not data:
                 break
             md5.update(data)
     return md5.hexdigest()
 
-# Hashes and compares two files to check if they are equal
-def isEqual(file1, file2):
-    f1Hash = hashfile(file1)
-    f2Hash = hashfile(file2)
 
-    if f1Hash == f2Hash:
+# Hashes and compares two files to check if they are equal
+def is_equal_file(file1, file2):
+    f1_hash = hash_file(file1)
+    f2_hash = hash_file(file2)
+
+    if f1_hash == f2_hash:
         return True
     return False
 
-def compareFolders():
-    sourceFiles = os.listdir(source_path)
-    replicaFiles = os.listdir(replica_path)
-    for file in sourceFiles:
-        if file not in replicaFiles:
-            replaceFile(file)
-            log("Added file " + file + " in replica folder")
-        if file in replicaFiles:
-            if not isEqual(os.path.join(source_path, file), os.path.join(replica_path, file)):
-                replaceFile(file)
-                log("Replaced file " + file + " in replica folder with updated version from source folder.")
-                #replicaFiles.remove(file)  # Does not delete the file, just removes it from replicaFiles array for shorter iterations
-
-
-        #sourceFiles.remove(file)        # Does not delete the file, just removes it from sourceFiles list for shorter iterations
-
-    for file in replicaFiles:
-        if file not in sourceFiles:     # If file is in replica but not in source, it is an old deleted file
-            os.remove(os.path.join(replica_path, file))
-            log("Removed file " + file + " from replica folder as it is no longer in source")
-
-def replaceFile(file):
-    sourceFilePath = os.path.join(source_path, file)
-    replicaFilePath = os.path.join(replica_path, file)
-    if os.path.exists(replicaFilePath):
-        os.remove(replicaFilePath)
-    shutil.copy2(sourceFilePath, replica_path)
 
 def log(text):
-    with open(log_path, 'a') as log:
-        log.write(text + '\n')
-    print(text)
+    message = "[" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "] " + text
+    with open(LOG_PATH, 'a') as log:
+        log.write(message + '\n')
+    print(message)
+
 
 if __name__ == '__main__':
     try:
